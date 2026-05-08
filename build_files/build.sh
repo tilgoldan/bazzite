b7e9938258fe0f88916fd9185b6980f9e73b2be6
Til Goldan <til@goldan.dev>
2026-05-08 07:44:36 +0200

    Enhance build script with additional packages and module build
    Added installation of additional packages and build steps for it87-extras kernel module.

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux ghostty iotop nethogs powertop waypipe amdgpu_top
 
 # Use a COPR Example:
 #
@@ -18,6 +18,40 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+### it87-extras (ITE IT8689E sensor chip support for Gigabyte B550 AORUS)
+KERNEL="$(rpm -q 'kernel' --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+KVERSION="$(rpm -q 'kernel' --queryformat '%{VERSION}-%{RELEASE}' | awk -F '[.]' '{print $1"."$2"."$3}')"
+
+# Install build deps + matching bazzite kernel-devel
+dnf5 -y group install development-tools
+curl -L -O "https://github.com/bazzite-org/kernel-bazzite/releases/download/${KVERSION}/kernel-devel-${KERNEL}.rpm"
+dnf5 -y install kernel-devel-${KERNEL}.rpm
+
+# Build the module
+git clone https://github.com/grandpares/it87.git /tmp/it87
+cd /tmp/it87
+make TARGET=$KERNEL clean
+make TARGET=$KERNEL modules
+
+# Install into the image
+mkdir -p /usr/lib/modules/${KERNEL}/extra/it87-extras
+xz -C crc32 it87-extras.ko
+cp it87-extras.ko.xz /usr/lib/modules/${KERNEL}/extra/it87-extras/
+depmod -a ${KERNEL}
+
+# Load at boot + options
+echo 'it87-extras' > /usr/lib/modules-load.d/it87-extras.conf
+echo 'options it87-extras ignore_resource_conflict=1' > /usr/lib/modprobe.d/it87-extras.conf
+
+# Persist kernel argument
+echo 'acpi_enforce_resources=lax' > /usr/lib/kernel/cmdline.d/it87.conf
+
+# Clean up build deps to keep image lean
+cd /
+rm -rf /tmp/it87 kernel-devel-${KERNEL}.rpm
+dnf5 -y group remove development-tools
+dnf5 -y remove git
 
 #### Example for enabling a System Unit File
 
